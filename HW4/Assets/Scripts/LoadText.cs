b992e21b0dd9d2f1583f888fad3c4253efdfdc52
marygeorgescu <mg3200@nyu.edu>
2019-03-11 02:33:23 -0400

    A script for colliding the player with things to score points.

---
+++ HW4/Assets/Scripts/LoadText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadText : MonoBehaviour
+{
+    public string txtfile = "test";
+
+    private string txtContents;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextAsset txtAssets = (TextAsset) Resources.Load("txtfile");
+        txtContents = txtAssets.text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void on GUI()
+    {
+       GUILayout.Label(txtContents);
+    }
+}
