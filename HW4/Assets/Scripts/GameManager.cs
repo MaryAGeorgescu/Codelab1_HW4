89c90a8fcf7235ebd30efd9e45f584c9a94aa3a4
marygeorgescu <mg3200@nyu.edu>
2019-03-11 03:12:51 -0400

    playing around with playerprefs

---
+++ HW4/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+//to create a class that there can be only one of
+
+{
+    public static GameManager manager;
+    public float health;
+
+    public float experiance;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //if there is no game controller make this the one and only game controller
+        if (manager == null)
+
+        {
+            DontDestroyOnLoad(gameObject);
+            manager =this
+        }
+        else if (manager!= this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
